41baae7fe54c41b255e8623dc8b63be6e7d2025e
Jeff Peeler <jpeeler@redhat.com>
2018-07-17 19:55:06 -0400

    add mutating webhook

---
+++ pkg/controller/podpreset/webhook/pki/gen-certs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SERVICE=podpreset-service
+NAMESPACE=podpreset-crd-system
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=127.0.0.1,$SERVICE,$SERVICE.kube-system,$SERVICE.$NAMESPACE,$SERVICE.$NAMESPACE.svc \
+  -profile=default \
+  webhook-csr.json | cfssljson -bare $SERVICE
+
+kubectl create secret tls $SERVICE-tls -n $NAMESPACE \
+  --cert=$SERVICE.pem \
+  --key=$SERVICE-key.pem
+
+base64 -w 0 ca.pem > ca.pem.base64
