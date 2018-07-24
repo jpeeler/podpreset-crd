9c4feaf580865f96e2e3c5695326e2674234ab7d
Jeff Peeler <jpeeler@redhat.com>
2018-07-24 16:46:30 -0400

    modify PodPresetSpec

--- pkg/apis/settings/v1alpha1/podpreset_types.go
+++ pkg/apis/settings/v1alpha1/podpreset_types.go
@@ -16,6 +16,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -26,6 +27,26 @@ import (
 type PodPresetSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Selector is a label query over a set of resources, in this case pods.
+	// Required.
+	Selector metav1.LabelSelector `json:"selector"`
+
+	// Env defines the collection of EnvVar to inject into containers.
+	// +optional
+	Env []v1.EnvVar `json:"env,omitempty"`
+
+	// EnvFrom defines the collection of EnvFromSource to inject into containers.
+	// +optional
+	EnvFrom []v1.EnvFromSource `json:"envFrom,omitempty"`
+
+	// Volumes defines the collection of Volume to inject into the pod.
+	// +optional
+	Volumes []v1.Volume `json:"volumes,omitempty"`
+
+	// VolumeMounts defines the collection of VolumeMount to inject into containers.
+	// +optional
+	VolumeMounts []v1.VolumeMount `json:"volumeMounts,omitempty"`
 }
 
 // PodPresetStatus defines the observed state of PodPreset
@@ -39,6 +60,10 @@ type PodPresetStatus struct {
 
 // PodPreset is the Schema for the podpresets API
 // +k8s:openapi-gen=true
+// +kubebuilder:resource:path=podpresets
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;watch;list;update
+// +kubebuilder:rbac:groups=,resources=events,verbs=create;patch;update
+// +kubebuilder:informers:group=apps,version=v1,kind=Deployment
 type PodPreset struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
